360e2d7d19e5388c818507ebc017ebee9aa21d3e
mike <mikemarakmike@gmail.com>
2026-07-10 17:03:16 +0300

    Update sokoban_env.py

---
+++ Deep Reinforcement Learning/src/environment/sokoban_env.py
@@ -0,0 +1,31 @@
+import gymnasium as gym
+from gymnasium import spaces
+
+class SokobanEnv(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def reset(self, seed=None, options=None):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
+
+    def _get_observation(self):
+        pass
+
+    def _encode_board(self):
+        pass
+
+    def _calculate_reward(self):
+        pass
+
+    def _is_done(self):
+        pass
